4c0b54eaae83f272590b23e5a91306d0d25273f4
valkarion <valkarion@gmail.com>
2019-01-19 23:08:06 +0100

    Added basic dependencies and empty main

---
+++ engine/application.cpp
@@ -0,0 +1,12 @@
+#define NOMINMAX				// supress windows.h min and max macros
+#define WIN32_LEAN_AND_MEAN		// don't include unnecessary stuff 
+#include <Windows.h>
+
+int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow )
+{
+
+
+
+	return 0;
+}
